7da688bc8839ec06f05baac9e2e3bb1931d40f46
Mustafa Demirkent <mustafa@saypr.com>
2023-11-20 21:53:04 +0000

    Update m.js

--- m.js
+++ m.js
@@ -1,1 +1,25 @@
-console.log('here');
+if(window.location.pathname.includes('/work')){
+  let querySelectorStr = '';
+  document.querySelectorAll('.page:not(.pinned)').forEach(function(outerEl){
+      if(outerEl.querySelector('media-item')){
+        if(querySelectorStr.length > 0){
+          querySelectorStr += ',';
+        }
+        querySelectorStr += "#" + outerEl.getAttribute('id') + ' media-item';
+      }
+  });
+
+  console.log(querySelectorStr);
+
+  let img = false;
+  document.querySelectorAll(querySelectorStr).forEach(function (item) {
+    img = false;
+    img = item.shadowRoot.querySelector('img');
+    if(img){
+      imgSrc = img.getAttribute("src");
+      img.setAttribute("data-lazyload", imgSrc);
+      img.setAttribute("src", "");
+      img.setAttribute("loading", "lazy");
+    }
+  });
+}
