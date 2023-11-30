914e4bb5e55bac94d461423c28b21dc0d65c8d2d
Mustafa Demirkent <mustafa@saypr.com>
2023-11-30 18:00:03 +0000

    Update m.js

--- m.js
+++ m.js
@@ -228,6 +228,19 @@ function culturePage() {
       }));
       
     }, 600);
+
+    document.onkeydown = function(evt) {
+      evt = evt || window.event;
+      var isEscape = false;
+      if ("key" in evt) {
+          isEscape = (evt.key === "Escape" || evt.key === "Esc");
+      } else {
+          isEscape = (evt.keyCode === 27);
+      }
+      if (isEscape) {
+          document.getElementById('D0470220286').style.display = 'none';
+      }
+  };
 }
 
 function restartPage(){
